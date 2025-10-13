cf149044ac6d80ff5e5751a8ba23ad3ecca3675f
Ann Kravets <kravetsanna78@gmail.com>
2025-10-13 13:44:35 +0300

    lesson 2 - internal modules

--- index.js
+++ index.js
@@ -1,41 +1,69 @@
-const http = require('node:http');
-const path = require('node:path');
-const readline = require('node:readline/promises');
+const fsPromises = require('node:fs/promises');
+const fs = require('node:fs');
+const EventEmitter = require('node:events');
+const os = require('node:os');
+const {exec} = require('child_process');
 
 const {foo: helperFoo} = require('./lessons/lesson1/helpers/helper');
+const path = require("node:path");
 
 
 const foo = async () => {
-    //example
-    // console.log(11111);
-    // helperFoo();
+   //FS
+   //  const pathToFile= path.join(__dirname, 'test.txt');
+   //  await fsPromises.writeFile(pathToFile, 'Hello World!\n');
+   //  const data = await fsPromises.readFile(pathToFile, 'utf-8');
+   //  console.log(data);
+   //  await fsPromises.appendFile(pathToFile, 'Some new data');
+   //  await fsPromises.mkdir(path.join(__dirname, 'new-folder'), { recursive: true });
+    //await fsPromises.mkdir(path.join(__dirname, 'new-folder', 'another-folder', 'another-another-folder')); - will be error
+    //await fsPromises.mkdir(path.join(__dirname, 'new-folder', 'another-folder', 'another-another-folder'), { recursive: true });
+    //await fsPromises.rm(path.join(__dirname, 'new-folder'), { recursive: true });
+    //await fsPromises.unlink(pathToFile); //delete file
+     //await fsPromises.rename(pathToFile, path.join(__dirname, 'new-folder', 'new-file.txt'));
+    // await fsPromises.copyFile(pathToFile, path.join(__dirname, 'new-folder', 'new-file.txt'));
+    // const stat = await fsPromises.stat(pathToFile);
+    // console.log(stat);
+    // console.log(stat.isDirectory());
+    // console.log(stat.isFile());
 
-    //HTTP
-    // const server = http.createServer((req, res) => {
-    //     res.writeHead(200, { 'Content-Type': 'application/json' });
-    //     res.end(JSON.stringify({
-    //         data: 'Hello World!',
-    //     }));
-    // });
-    //
-    // server.listen(3000);
+   //Streams
+   //  const pathToFile = path.join(__dirname, 'science_and_art.pptx');
+   //  const readStream = fs.createReadStream(pathToFile);
+   //  const writeStream = fs.createWriteStream(path.join(__dirname, 'new-big-file.pptx'));
+   //  readStream.on('data', (chunk) => {
+   //      console.log('chunk', chunk.length);
+   //      writeStream.write(chunk);
+   //  });
+   //
+   //  another variant
+   //  readStream.pipe(writeStream);
 
-    //Path
-    // const pathToFile = __filename;
-    // console.log(path.dirname(pathToFile));
-    // console.log(path.extname(pathToFile));
-    // console.log(path.basename(pathToFile));
-    // console.log(path.parse(pathToFile));
-    // console.log(path.isAbsolute(pathToFile));
+   //Events
+   //  const emitter = new EventEmitter();
+   //  emitter.once('event', (...args) => {
+   //      console.log('__________________');
+   //      console.log('Event 1 Happened');
+   //      console.log(args);
+   //      console.log('__________________');
+   //  })
+   //  emitter.on('event', (...args) => {
+   //      console.log('Event 2 Happened');
+   //      console.log(args);
+   //  })
+   //  emitter.emit('event', 'Hello)!', 345, 44);
+   //  emitter.emit('event');
+   //  emitter.emit('event', 555);
 
-    //readLine
-    const rlInstance = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    })
-    const name = await rlInstance.question('Name?');
-    console.log(`Your name is ${name}`);
-    process.exit(0);
+   //OS
+    console.log(os.arch());
+    console.log(os.cpus());
+    console.log(os.totalmem() / 1024 / 1024 / 1024, 'gb');
+    console.log(os.freemem() / 1024 / 1024 / 1024, 'gb');
+    console.log(os.homedir());
+    console.log(os.hostname());
+    console.log(os.platform());
+    console.log(os.userInfo());
 
 
 }
