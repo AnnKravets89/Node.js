fac160de72492d737b88d24e62062ae1029fbf53
Ann Kravets <kravetsanna78@gmail.com>
2025-10-14 16:05:05 +0300

    lesson 3

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const express = require('express');
 
+const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+app.get('/users', (req, res) => {
+    res.send('Hello World!');
+});
+app.post('/users', (req, res) => {
+    res.send('Hello Everybody!');
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
