e830eb69d3552dbfce531e3264c2c3d6730da231
Ann Kravets <kravetsanna78@gmail.com>
2025-10-16 14:55:24 +0300

    homework 3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {saveUsersToFile} = require('./service/usersService');
+const { readUsersFromFile, saveUsersToFile } = require('./service/usersService');
 
 const app = express();
 
@@ -8,72 +8,104 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/users', async (req, res) => {
     try {
-        const users = await saveUsersToFile();
+        const users = await readUsersFromFile();
         res.json(users);
     } catch (e) {
         res.status(500).send(e.message);
     }
 });
 
-app.post('/users', (req, res) => {
-     try {
-         const {name, email, password} = req.body;
-         //TODO validate  data
-         const id = users[users.length - 1].id + 1;
-         const newUser = {id, name, email, password};
-         users.push(newUser);
-         res.status(201).send(newUser);
-     } catch (e) {
-         res.status(500).send(e.message);
-     }
-});
-
-app.put('/users/:userId', (req, res) => {
+app.get('/users/:userId', async (req, res) => {
     try {
-        const userId = Number(req.params.userId);
-        const userIndex = users.findIndex(user => user.id === userId);
-            if (userIndex === -1) {
-                return res.status(404).send('User not found');
-            }
-        const {name, email, password} = req.body;
-        //TODO validate data
-        // users[userIndex] = {...users[userIndex], name, email, password};
-        users[userIndex].name = name;
-        users[userIndex].email = email;
-        users[userIndex].password = password;
-        res.status(201).send(users[userIndex]);
-    } catch (e) {
-        res.status(500).send(e.message);
-    }
-});
-
-app.delete('/users/:userId', (req, res) => {
-    try {
-        const userId = Number(req.params.userId);
-        const userIndex = users.findIndex(user => user.id === userId);
-            if (userIndex === -1) {
-                return res.status(404).send('User not found');
-            }
-        users.splice(userIndex, 1);
-        res.sendStatus(204);
-    } catch (e) {
-        res.status(500).send(e.message);
-    }
-});
-
-app.get('/users/:userId', (req, res) => {
-    try {
+        const users = await readUsersFromFile();
         const userId = Number(req.params.userId);
         const user = users.find((user) => user.id === userId);
-            if (!user) {
-                return res.status(404).send('User not found');
-            }
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
         res.send(user);
     } catch (e) {
         res.status(500).send(e.message);
     }
 });
 
+app.post('/users', async (req, res) => {
+     try {
+         const users = await readUsersFromFile();
+         const {name, email, password} = req.body;
+
+             if (name.length < 3) {
+                 throw new Error('Name should be at least 3 characters long');
+             }
+             if (!email || !email.includes("@")) {
+                 throw new Error("Email is required and should include @");
+             }
+             if (password.length < 6) {
+                 throw new Error(
+                     "Password  should be at least 6 characters long");
+             }
+
+         const id = users.length ? users[users.length - 1].id + 1 : 1;
+         const newUser = {id, name, email, password};
+         users.push(newUser);
+         await saveUsersToFile(users);
+         res.status(201).json(newUser);
+     } catch (e) {
+         res.status(500).send(e.message);
+     }
+});
+
+app.put('/users/:userId', async (req, res) => {
+    try {
+        const userId = Number(req.params.userId);
+        const {name, email, password} = req.body;
+
+            if (name.length < 3) {
+                throw new Error('Name should be at least 3 characters long');
+            }
+            if (!email || !email.includes("@")) {
+                throw new Error("Email is required and should include @");
+            }
+            if (password.length < 6) {
+                throw new Error(
+                    "Password  should be at least 6 characters long");
+            }
+
+        const users = await readUsersFromFile();
+
+        const userIndex = users.findIndex(user => user.id === userId);
+        if (userIndex === -1) {
+            return res.status(404).send('User not found');
+        }
+
+        users[userIndex].name = name;
+        users[userIndex].email = email;
+        users[userIndex].password = password;
+
+        await saveUsersToFile(users);
+        res.status(201).send(users[userIndex]);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
+
+app.delete('/users/:userId', async (req, res) => {
+    try {
+        const userId = Number(req.params.userId);
+        const users = await readUsersFromFile();
+
+        const userIndex = users.findIndex(user => user.id === userId);
+            if (userIndex === -1) {
+                return res.status(404).send('User not found');
+            }
+        users.splice(userIndex, 1);
+
+        await saveUsersToFile(users);
+        res.sendStatus(204);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
--- service/usersService.js
+++ service/usersService.js
@@ -1,28 +1,37 @@
 const fsPromises = require('node:fs/promises');
 const path = require('node:path');
 
-const saveUsersToFile = async () => {
+const users = [
+    {id: 1, name: 'Maksym', email: 'feden@gmail.com', password: 'qwe123'},
+    {id: 2, name: 'Alina', email: 'alindosik@gmail.com', password: 'ert345'},
+    {id: 3, name: 'Anna', email: 'ann43@gmail.com', password: 'ghj393'},
+    {id: 4, name: 'Tamara', email: 'tomochka23@gmail.com', password: 'afs787'},
+    {id: 5, name: 'Dima', email: 'taper@gmail.com', password: 'rtt443'},
+    {id: 6, name: 'Rita', email: 'torpeda@gmail.com', password: 'vcx344'},
+    {id: 7, name: 'Denis', email: 'denchik@gmail.com', password: 'sdf555'},
+    {id: 8, name: 'Sergey', email: 'BigBoss@gmail.com', password: 'ccc322'},
+    {id: 9, name: 'Angela', email: 'lala@gmail.com', password: 'cdd343'},
+    {id: 10, name: 'Irina', email: 'irka7@gmail.com', password: 'kkk222'},
+];
+
+const pathToFile = path.join(__dirname, '../users-db.json');
+
+const readUsersFromFile = async () => {
     try {
-        const users = [
-            {id: 1, name: 'Maksym', email: 'feden@gmail.com', password: 'qwe123'},
-            {id: 2, name: 'Alina', email: 'alindosik@gmail.com', password: 'ert345'},
-            {id: 3, name: 'Anna', email: 'ann43@gmail.com', password: 'ghj393'},
-            {id: 4, name: 'Tamara', email: 'tomochka23@gmail.com', password: 'afs787'},
-            {id: 5, name: 'Dima', email: 'taper@gmail.com', password: 'rtt443'},
-            {id: 6, name: 'Rita', email: 'torpeda@gmail.com', password: 'vcx344'},
-            {id: 7, name: 'Denis', email: 'denchik@gmail.com', password: 'sdf555'},
-            {id: 8, name: 'Sergey', email: 'BigBoss@gmail.com', password: 'ccc322'},
-            {id: 9, name: 'Angela', email: 'lala@gmail.com', password: 'cdd343'},
-            {id: 10, name: 'Irina', email: 'irka7@gmail.com', password: 'kkk222'},
-        ];
-
-        const pathToFile = path.join(__dirname, '../users-db.json');
-        await fsPromises.writeFile(pathToFile, JSON.stringify(users, null, 2), 'utf8');
-        const fileData = await fsPromises.readFile(pathToFile, 'utf8');
-
-        return JSON.parse(fileData);
+        const data = await fsPromises.readFile(pathToFile, 'utf8');
+        return JSON.parse(data);
     } catch (e) {
-        console.log('Error creating users');
+        if (e.code === 'ENOENT') {
+            await saveUsersToFile(users);
+            return users;
+        }
+        console.log('Users not found', e.message);
+        return [];
     }
+};
+
+const saveUsersToFile = async (users) => {
+    const data = JSON.stringify(users, null, 2);
+    await fsPromises.writeFile(pathToFile, data, 'utf8');
 }
-module.exports =  {saveUsersToFile};
+module.exports =  { readUsersFromFile, saveUsersToFile };
