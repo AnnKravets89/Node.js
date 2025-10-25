4e4e0f285be66e543193aeb236c729f841f5aa4a
Ann Kravets <kravetsanna78@gmail.com>
2025-10-25 17:18:14 +0300

    homework 5

--- src/repositories/token.repository.ts
+++ src/repositories/token.repository.ts
@@ -2,7 +2,7 @@ import { IToken } from "../interfaces/token.interface";
 import { Token } from "../models/token.model";
 
 class TokenRepository {
-    public create(dto: any): Promise<IToken> {
+    public create(dto: IToken): Promise<IToken> {
         return Token.create(dto);
     }
 
