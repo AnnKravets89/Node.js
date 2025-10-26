c9a652fd6ca8fdd1ac51816ca81927245253740b
Ann Kravets <kravetsanna78@gmail.com>
2025-10-26 10:46:00 +0200

    lesson 6 nodemailer, handlerbars

---
+++ src/services/email.service.ts
@@ -0,0 +1,52 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
+import handlebars from "handlebars";
+import nodemailer, { Transporter } from "nodemailer";
+
+import { config } from "../configs/config";
+
+class EmailService {
+    private transporter: Transporter;
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: config.EMAIL_USER,
+                pass: config.EMAIL_PASSWORD,
+            },
+        });
+    }
+    private async _renderTemplate(
+        templateName: string,
+        context: Record<string, any>,
+    ): Promise<string> {
+        const layoutSourse = await fs.readFile(
+            path.join(process.cwd(), "src", "templates", "base.hbs"),
+            "utf-8",
+        );
+        const layoutTemplate = handlebars.compile(layoutSourse);
+        const templateSourse = await fs.readFile(
+            path.join(process.cwd(), "src", "templates", `${templateName}.hbs`),
+            "utf-8",
+        );
+        const childTemplate = handlebars.compile(templateSourse);
+        const childHtml = childTemplate(context);
+        return layoutTemplate({ body: childHtml });
+    }
+
+    public async sendEmail(
+        to: string,
+        subject: string,
+        templateName: string,
+        context: Record<string, any>,
+    ): Promise<void> {
+        await this.transporter.sendMail({
+            to,
+            subject,
+            html: await this._renderTemplate(templateName, context),
+        });
+    }
+}
+
+export const emailService = new EmailService();
