8693145b49f149b5829fa5c0e989c728560529a1
Ann Kravets <kravetsanna78@gmail.com>
2025-10-27 14:27:01 +0200

    homework 6

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,6 @@
 import { StatusCodesEnum } from "../enums/status-codes.enum";
 import { ApiError } from "../errors/api.error";
-import {
-    IUser,
-    IUserCreateDTO,
-    IUserUpdateDTO,
-} from "../interfaces/user.interface";
+import { IUser, IUserCreateDTO } from "../interfaces/user.interface";
 import { userRepository } from "../repositories/user.repository";
 
 class UserService {
@@ -27,7 +23,7 @@ class UserService {
 
     public async updateById(
         userId: string,
-        user: IUserUpdateDTO,
+        user: Partial<IUser>,
     ): Promise<IUser> {
         const data = await userRepository.getById(userId);
         if (!data) {
@@ -65,6 +61,9 @@ class UserService {
 
     public unBlockUsers(user_id: string): Promise<IUser> {
         return userRepository.unBlockUser(user_id);
+    }
+    public getByEmail(email: string): Promise<IUser> {
+        return userRepository.getByEmail(email);
     }
 }
 
