a9c48c9bec7057d93e60f97775e0b7d4b5392629
Ann Kravets <kravetsanna78@gmail.com>
2025-11-09 11:38:21 +0200

    homework 11 - send letters with spam to all users

--- backend/src/crons/index.ts
+++ backend/src/crons/index.ts
@@ -1,7 +1,5 @@
-import { spamCron } from "./spam.cron";
-
 export const cronRunner = async () => {
     //testCron.start();
     //removeOldTokensCron.start();
-    spamCron.start();
+    //spamCron.start();
 };
